9f5b8e0cb1f6440bbd46574d6680de830d188880
Ian Danas <ianmdanas@gmail.com>
2023-05-29 12:29:27 -0700

    adding chatRoom server config

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const express = require('express');
-const allRoutes = require('./controllers');
+// const allRoutes = require('./controllers');
 const cors = require("cors")
-const routes = require('./routes');
+// const routes = require('./routes');
 const db = require('./config/connection');
+
 
 const cwd = process.cwd();
 
 const PORT = process.env.PORT || 3001;
-const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(routes);
+const app = express();
+const server = require('http').Server(app)
+
+const io = require('socket.io')(server)
+// const ioConnection = require('./sockets/ioConnection')
+
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => {
+  server.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
 });
